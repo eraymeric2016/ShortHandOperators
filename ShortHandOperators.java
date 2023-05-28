78e5032e06fc944b2e078b2fb3a2c0a504681e98
eraymeric2016 <125488259+eraymeric2016@users.noreply.github.com>
2023-05-28 14:39:22 +0300

    Add files via upload

---
+++ ShortHandOperators.java
@@ -0,0 +1,38 @@
+package week_04;
+
+public class ShortHandOperators {
+    public static void main(String[] args) {
+
+        int a=10;
+        int result=a+10;
+        a=a+10;
+
+
+        System.out.println(a);
+
+        int b=0;
+
+        b+=10;
+
+        System.out.println("b = " + b);
+
+        b*=2;
+
+        System.out.println("b = " + b);
+
+        b/=5;
+
+        System.out.println("b = " + b);
+
+        b-=4;
+        System.out.println("b = " + b);
+
+        int c=10;
+        c+=10;
+
+        System.out.println("c = " + c);
+
+
+
+    }
+}
